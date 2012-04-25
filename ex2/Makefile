59048be618bc6b87c603432697a7a30ecadf1038
Grant Austin <gaustin@gmail.com>
2012-04-25 07:14:29 -0600

    Make - ex2

---
+++ ex2/Makefile
@@ -0,0 +1,7 @@
+CFLAGS=-Wall -g
+
+all:
+	cc -Wall -g    ex1.c   -o ex1
+
+clean:
+	rm -f ex1
---
+++ ex2/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int i = 6;
+
+  while(i--) {
+    puts("Hello World.");
+  }
+
+  return 0;
+}
